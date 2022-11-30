1c8090e1a4a1d19c31e92c7f9965aa08c378ed55
Michael ilkanayev <93651794+michaelilkanayev1997@users.noreply.github.com>
2022-12-01 00:03:26 +0200

    Firebase db Added

--- src/App.js
+++ src/App.js
@@ -1,45 +1,19 @@
-import React, { useEffect } from "react";
-import ScrollToTop from "./components/ScrollToTop.jsx";
-import Hero from "./components/Hero.jsx";
-import Navbar from "./components/Navbar.jsx";
-import Services from "./components/Services.jsx";
-import Recommend from "./components/Recommend.jsx";
-import Testimonials from "./components/Testimonials.jsx";
-import Footer from "./components/Footer.jsx";
-import ScrollReveal from "scrollreveal";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+import Login from "./components/Login.jsx";
+import { AuthContextProvider } from "./components/context/AuthContext.jsx";
+import Home from "./components/Home.jsx";
 
 export default function App() {
-  useEffect(() => {
-    const sr = ScrollReveal({
-      //Animations
-      origin: "top",
-      distance: "80px",
-      duration: 2000,
-      reset: true,
-    });
-    sr.reveal(
-      `
-      nav,
-      #hero,
-      #services,
-      #recommend,
-      #testimonials,
-      footer`,
-      {
-        opacity: 0,
-        interval: 300,
-      }
-    );
-  }, []);
   return (
-    <>
-      <ScrollToTop />
-      <Navbar />
-      <Hero />
-      <Services />
-      <Recommend />
-      <Testimonials />
-      <Footer />
-    </>
+    <div>
+      <AuthContextProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Login" element={<Login />} />
+        </Routes>
+      </AuthContextProvider>
+    </div>
   );
 }
---
+++ src/components/Firebase.jsx
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB-UhzdSDnF5UPriiVuqUDWWbO_bAmPByE",
+  authDomain: "travelgo-20e9e.firebaseapp.com",
+  projectId: "travelgo-20e9e",
+  storageBucket: "travelgo-20e9e.appspot.com",
+  messagingSenderId: "22056990411",
+  appId: "1:22056990411:web:278ed4baeea4176af5cd4e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
